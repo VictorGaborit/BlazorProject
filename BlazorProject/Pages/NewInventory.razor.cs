01d526cbc6e863bf839e9d07e5a29f91780a96ea
Charlie PERRON D'ARC <Charlie.PERRON_D_ARC@etu.uca.fr>
2022-12-31 12:51:40 +0100

    Implémente la fonction de recherche

--- BlazorProject/Pages/NewInventory.razor.cs
+++ BlazorProject/Pages/NewInventory.razor.cs
@@ -7,9 +7,14 @@ namespace BlazorProject.Pages
 {
     public partial class NewInventory
     {
-        string TextValue { get; set; }
+        string? TextValue { get; set; }
+        
+        bool haveAnyItem = true;
+        
+        private List<Item> itemsFound = new List<Item>();
 
         private List<Item> items;
+        private List<Item> allItems;
 
         private int totalItem;
 
@@ -29,12 +34,45 @@ namespace BlazorProject.Pages
             if (!e.CancellationToken.IsCancellationRequested)
             {
                 items = await DataService.List(e.Page, e.PageSize);
+                allItems = await DataService.List(0, 100000000);
                 totalItem = await DataService.Count();
             }
         }
-        private void search()
+        private void searching()
         {
-
+            if (TextValue is null || TextValue.Length == 0)
+            {
+                itemsFound.Clear();
+                itemsFound.AddRange(items);
+                haveAnyItem = true;
+            }
+            else
+            {
+                itemsFound.Clear();
+                foreach (var item in allItems)
+                {
+                    if ((item.Name.ToLower().Contains(TextValue.ToLower())) && !itemsFound.Contains(item))
+                    {
+                        if (item.Name.ToLower().Equals(TextValue.ToLower()))
+                        {
+                            itemsFound.Insert(0, item);
+                        }
+                        else
+                        {
+                            itemsFound.Add(item);
+                        }
+                            
+                    }
+                }
+            }
+            if(itemsFound.Count > 0)
+            {
+                haveAnyItem = true;
+            }
+            else
+            {
+                haveAnyItem = false;
+            }
         }
     }
 }
